c5f97c367c761e21fbaa94db783576f9f18fa686
Cuevas113 <luisgerardocuevas5@gmail.com>
2022-07-20 12:14:52 -0500

    Revert "Pequeños cambios, nueva actividad"
    This reverts commit b66bb4c9cc67b47c6abff89cff747370afa6e489.

--- app/src/main/java/com/example/refineria/ANTIGENO/ANTIGENO_subir_resultado.kt
+++ app/src/main/java/com/example/refineria/ANTIGENO/ANTIGENO_subir_resultado.kt
@@ -1,47 +1,20 @@
 package com.example.refineria.ANTIGENO
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
 import com.example.refineria.R
-import kotlinx.android.synthetic.main.activity_antigeno_subir_resultado.*
 
 class ANTIGENO_subir_resultado : AppCompatActivity() {
-    val bundle = intent.extras
-    val numero = bundle?.getString("NUMERO")
-    val nombre = bundle?.getString("NOMBRE")
-    val prefolio = bundle?.getString("PREFOLIO")
-
     override fun onCreate(savedInstanceState: Bundle?) {
-
-
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_antigeno_subir_resultado)
 
+        val bundle = intent.extras
+        val numero = bundle?.getString("NUMERO")
+        val nombre = bundle?.getString("NOMBRE")
+        val prefolio = bundle?.getString("PREFOLIO")
 
 
-
-
-        //Enviar resultado
-        PositivoButton.setOnClickListener {
-            form("Positivo")
-        }
-        NegativoButton.setOnClickListener {
-            form("Negativo")
-        }
-
-    }
-    fun form(resultado:String){
-        val intent = Intent(this, ANTIGENO_resultado_formulario::class.java).apply {
-            putExtra("NUMERO", numero)
-            putExtra("NOMBRE", nombre)
-            putExtra("PREFOLIO", prefolio)
-            putExtra("RESULTADO", resultado)
-        }
-        this.startActivity(intent)
-        Toast.makeText(this, "Se a seleccionado: $resultado", Toast.LENGTH_SHORT).show()
 
     }
 }
